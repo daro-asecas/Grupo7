adff7cb8eccf0c5de86fc65fba49a8f60446ad92
RioLev61 <89041493+RioLev61@users.noreply.github.com>
2023-07-02 16:45:51 -0300

    Correccion en  contacto

--- src/components/routes/Contacto.jsx
+++ src/components/routes/Contacto.jsx
@@ -4,6 +4,19 @@ import Form from 'react-bootstrap/Form';
 
 export function Contacto() {
     return (
+      <div>
+       
+        <h2>QUIENES SOMOS	</h2>	
+                  									
+																						
+         <p> SOMOS LA PRIMERA MULTIMARCA FEDERAL DEDICADA A LA COMERCIALIZACIÓN DE UNIDADES 0KM NACIONALES E IMPORTADAS.																					                                                                            
+          NUESTRA CASA MATRÍZ FUE FUNDADA EN LA CIUDAD DE BUENOS AIRES EN MARZO DE 1967.																					
+          EN LA CIUDAD DE USHUAIA, NUESTRA ÚLTIMA APERTURA, ESTAMOS DESDE JULIO DEL 2008 																					
+          NUESTRO SERVICIO DE ATENCIÓN POST VENTA CUENTA CON LA MEJOR TECNOLOGÍA INSTALADA DEL PAÍS																					
+          LOS REPUESTOS Y ACCESORIOS DE EXCELENCIA NOS UBICAN EN EL 1º PUESTO EN EL MERCADO AUTOMOTOR																					
+          LA CORDIALIDAD Y EL PROFESIONALISMO SON NUESTROS ATRIBUTOS FUNDAMENTALES		</p>																			
+        
+     <div className='form'>
       <Form>
         <br /><br /><br /><br /><br />
       <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -24,5 +37,7 @@ export function Contacto() {
       <br />
       <br /><br /><br />
     </Form>
+    </div>
+    </div>
     );
   }
